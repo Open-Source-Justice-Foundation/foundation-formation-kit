0bdcff38a589141dfdcda4ea7bba8df04882904a
J the Code Monkey <jaythecodemonkey.7vk7i@slmail.me>
2025-02-28 13:29:27 -0500

    fix: downgrade argon2 version and remove experimental object from next config

--- next.config.mjs
+++ next.config.mjs
@@ -8,14 +8,6 @@ const nextConfig = {
       },
     ],
   },
-  experimental: {
-    // Fixes issues with argon2 and vercel
-    // - https://github.com/vercel/next.js/discussions/65978
-    // - https://github.com/ranisalt/node-argon2/issues/402#issuecomment-2073016949
-    outputFileTracingIncludes: {
-      "/": ["../node_modules/argon2/prebuilds/linux-x64/*"],
-    },
-  },
 };
 
 export default nextConfig;
