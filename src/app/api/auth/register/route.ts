d6cbfbb86c3ef64d5b4ba07f31b6b23d6cadd957
J the Code Monkey <jaythecodemonkey.7vk7i@slmail.me>
2025-02-28 08:10:29 -0500

    fix: remove email exists error handling and password hashing to try to resolve deployment error

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,5 +1,5 @@
 import { neon } from "@neondatabase/serverless";
-import { saltAndHashPassword } from "~/lib/auth/utils";
+// import { saltAndHashPassword } from "~/lib/auth/utils";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest): Promise<NextResponse> {
@@ -7,7 +7,7 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
     const sql = neon(process.env.DATABASE_URL);
 
     try {
-      const { email, password } = await request.json();
+      const { email } = await request.json();
 
       // TODO
       // Validate email and password here on the server using zod
@@ -20,22 +20,22 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
 
       console.log("emailExistsResponse", emailExistsResponse);
 
-      if (emailExistsResponse === undefined) {
-        throw new Error("Failed to select user from database");
-      } else if (emailExistsResponse.length !== 1) {
-        throw new Error("Failed to check if email exists in database");
-      } else if (!emailExistsResponse[0].hasOwnProperty("exists")) {
-        throw new Error("Failed to check for exists property");
-      } else if (emailExistsResponse[0].exists !== false) {
-        throw new Error("Email already exists");
-      }
+      // if (emailExistsResponse === undefined) {
+      //   throw new Error("Failed to select user from database");
+      // } else if (emailExistsResponse.length !== 1) {
+      //   throw new Error("Failed to check if email exists in database");
+      // } else if (!emailExistsResponse[0].hasOwnProperty("exists")) {
+      //   throw new Error("Failed to check for exists property");
+      // } else if (emailExistsResponse[0].exists !== false) {
+      //   throw new Error("Email already exists");
+      // }
 
-      const passwordHash = await saltAndHashPassword(password);
-
-      await sql("INSERT INTO users (email, password_hash) VALUES ($1, $2)", [
-        email,
-        passwordHash,
-      ]);
+      // const passwordHash = await saltAndHashPassword(password);
+      //
+      // await sql("INSERT INTO users (email, password_hash) VALUES ($1, $2)", [
+      //   email,
+      //   passwordHash,
+      // ]);
     } catch (err) {
       // TODO
       // Don't log the err value, do something else with it to avoid deployment error
