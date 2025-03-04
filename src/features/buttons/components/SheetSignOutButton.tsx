d01305c4d495284265e2f122ad56846f546e8d87
J the Code Monkey <jaythecodemonkey.7vk7i@slmail.me>
2025-03-04 15:10:33 -0500

    feat: add redirect to homepage on sign out

--- src/features/buttons/components/SheetSignOutButton.tsx
+++ src/features/buttons/components/SheetSignOutButton.tsx
@@ -9,7 +9,7 @@ export function SheetSignOutButton() {
       type="button"
       className="h-9 justify-start px-3 py-1.5 text-base focus-visible:outline-ring/50 focus-visible:ring-0 focus-visible:ring-offset-0"
       variant="ghost"
-      onClick={() => signOut()}
+      onClick={() => signOut({ redirect: true, redirectTo: "/" })}
     >
       Sign out
     </Button>
--- src/features/buttons/components/SignOutButton.tsx
+++ src/features/buttons/components/SignOutButton.tsx
@@ -9,7 +9,7 @@ export function SignOutButton() {
       type="button"
       className="h-9 p-0 text-base text-link-foreground hover:no-underline focus-visible:ring-0 focus-visible:ring-offset-0"
       variant="link"
-      onClick={() => signOut()}
+      onClick={() => signOut({ redirect: true, redirectTo: "/" })}
     >
       Sign out
     </Button>
