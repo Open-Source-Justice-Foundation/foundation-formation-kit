0f82c44d380781f555a6458dd6175f213b32ba09
J the Code Monkey <jaythecodemonkey.7vk7i@slmail.me>
2025-03-12 10:48:03 -0400

    feat: add error handling and disabled state to sign out btns

--- src/features/buttons/components/SheetSignOutButton.tsx
+++ src/features/buttons/components/SheetSignOutButton.tsx
@@ -1,15 +1,41 @@
 "use client";
 
+import { useState } from "react";
+
 import { Button } from "~/components/ui/button";
 import { signOut } from "next-auth/react";
+import { toast } from "sonner";
 
 export function SheetSignOutButton() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  async function handleSignOutOnClick() {
+    setIsLoading(true);
+    try {
+      const signOutResponse = await signOut({
+        redirect: true,
+        redirectTo: "/",
+      });
+
+      if (signOutResponse !== undefined) {
+        throw new Error("Failed to redirect to homepage");
+      }
+    } catch (err) {
+      // TODO
+      // Don't log the err value, do something else with it to avoid deployment error
+      console.error(err);
+      toast.error("Logout error");
+      setIsLoading(false);
+    }
+  }
+
   return (
     <Button
       type="button"
-      className="h-9 justify-start px-3 py-1.5 text-base focus-visible:outline-ring/50 focus-visible:ring-0 focus-visible:ring-offset-0"
       variant="ghost"
-      onClick={() => signOut({ redirect: true, redirectTo: "/" })}
+      className="h-9 justify-start px-3 py-1.5 text-base focus-visible:outline-ring/50 focus-visible:ring-0 focus-visible:ring-offset-0"
+      onClick={handleSignOutOnClick}
+      disabled={isLoading}
     >
       Sign out
     </Button>
--- src/features/buttons/components/SignOutButton.tsx
+++ src/features/buttons/components/SignOutButton.tsx
@@ -4,21 +4,38 @@ import { useState } from "react";
 
 import { Button } from "~/components/ui/button";
 import { signOut } from "next-auth/react";
+import { toast } from "sonner";
 
 export function SignOutButton() {
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
+  async function handleSignOutOnClick() {
+    setIsLoading(true);
+    try {
+      const signOutResponse = await signOut({
+        redirect: true,
+        redirectTo: "/",
+      });
+
+      if (signOutResponse !== undefined) {
+        throw new Error("Failed to redirect to homepage");
+      }
+    } catch (err) {
+      // TODO
+      // Don't log the err value, do something else with it to avoid deployment error
+      console.error(err);
+      toast.error("Logout error");
+      setIsLoading(false);
+    }
+  }
+
   return (
     <Button
       type="button"
-      className="h-9 p-0 text-base text-link-foreground hover:no-underline focus-visible:ring-0 focus-visible:ring-offset-0"
       variant="link"
+      className="h-9 p-0 text-base text-link-foreground hover:no-underline focus-visible:ring-0 focus-visible:ring-offset-0"
+      onClick={handleSignOutOnClick}
       disabled={isLoading}
-      onClick={async () => {
-        setIsLoading(true);
-        await signOut({ redirect: true, redirectTo: "/" });
-        setIsLoading(false);
-      }}
     >
       Sign out
     </Button>
