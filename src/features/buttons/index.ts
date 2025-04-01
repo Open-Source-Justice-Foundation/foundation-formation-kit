61402d32df2dddb99807f639c5de573bf2bed4e9
J the Code Monkey <jaythecodemonkey.7vk7i@slmail.me>
2025-04-01 10:16:53 -0400

    chore: remove unused sign out btn component

--- src/features/buttons/index.ts
+++ src/features/buttons/index.ts
@@ -1,3 +1,2 @@
 export { GetStartedButton } from "./components/GetStartedButton";
-export { SignOutButton } from "./components/SignOutButton";
 export { SheetSignOutButton } from "./components/SheetSignOutButton";
