af6f72dff14373e66d01028cbacbe2f143bc2b8d
J the Code Monkey <jaythecodemonkey.7vk7i@slmail.me>
2025-04-03 22:21:00 -0400

    fix: remove sheet close from header sheet external links to prevent sheet flashing when navigating back to the site

--- src/features/header/components/HeaderSheetExternalLinks.tsx
+++ src/features/header/components/HeaderSheetExternalLinks.tsx
@@ -1,4 +1,3 @@
-import { SheetClose } from "~/components/ui/sheet";
 import { HEADER_SHEET_ICON_BASE_SIZE } from "~/features/header/constants/constants";
 import {
   FFK_DOCS_URL,
@@ -10,51 +9,42 @@ import { BookText, Github, HandHelping } from "lucide-react";
 export function HeaderSheetExternalLinks() {
   return (
     <>
-      <SheetClose asChild>
-        <a
-          href={FFK_DOCS_URL}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="rounded-md px-3 py-1.5 hover:bg-accent hover:text-accent-foreground"
-        >
-          <div className="flex items-center gap-2 text-base">
-            <BookText size={HEADER_SHEET_ICON_BASE_SIZE} aria-hidden="true" />
-            <span className="sr-only">{"Docs"}</span>
-            <span className="font-medium">Docs</span>
-          </div>
-        </a>
-      </SheetClose>
-      <SheetClose asChild>
-        <a
-          href={SUPPORT_EMAIL_URI}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="rounded-md px-3 py-1.5 hover:bg-accent hover:text-accent-foreground"
-        >
-          <div className="flex items-center gap-2 text-base">
-            <HandHelping
-              size={HEADER_SHEET_ICON_BASE_SIZE}
-              aria-hidden="true"
-            />
-            <span className="sr-only">{"Support"}</span>
-            <span className="font-medium">Support</span>
-          </div>
-        </a>
-      </SheetClose>
-      <SheetClose asChild>
-        <a
-          href={FFK_GITHUB_URL}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="rounded-md px-3 py-1.5 hover:bg-accent hover:text-accent-foreground"
-        >
-          <div className="flex items-center gap-2 text-base">
-            <Github size={HEADER_SHEET_ICON_BASE_SIZE} aria-hidden="true" />
-            <span className="sr-only">{"GitHub"}</span>
-            <span className="font-medium">GitHub</span>
-          </div>
-        </a>
-      </SheetClose>
+      <a
+        href={FFK_DOCS_URL}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="rounded-md px-3 py-1.5 hover:bg-accent hover:text-accent-foreground"
+      >
+        <div className="flex items-center gap-2 text-base">
+          <BookText size={HEADER_SHEET_ICON_BASE_SIZE} aria-hidden="true" />
+          <span className="sr-only">{"Docs"}</span>
+          <span className="font-medium">Docs</span>
+        </div>
+      </a>
+      <a
+        href={SUPPORT_EMAIL_URI}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="rounded-md px-3 py-1.5 hover:bg-accent hover:text-accent-foreground"
+      >
+        <div className="flex items-center gap-2 text-base">
+          <HandHelping size={HEADER_SHEET_ICON_BASE_SIZE} aria-hidden="true" />
+          <span className="sr-only">{"Support"}</span>
+          <span className="font-medium">Support</span>
+        </div>
+      </a>
+      <a
+        href={FFK_GITHUB_URL}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="rounded-md px-3 py-1.5 hover:bg-accent hover:text-accent-foreground"
+      >
+        <div className="flex items-center gap-2 text-base">
+          <Github size={HEADER_SHEET_ICON_BASE_SIZE} aria-hidden="true" />
+          <span className="sr-only">{"GitHub"}</span>
+          <span className="font-medium">GitHub</span>
+        </div>
+      </a>
     </>
   );
 }
