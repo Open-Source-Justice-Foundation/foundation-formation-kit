47a5d2c512b11d5196df04733d55844635643cd4
J the Code Monkey <jaythecodemonkey.7vk7i@slmail.me>
2025-06-06 16:36:32 -0400

    feat: prevent linking multiple github accounts to the same user from the profile page

--- src/app/api/auth/is-link-oauth-allowed-from-profile/route.ts
+++ src/app/api/auth/is-link-oauth-allowed-from-profile/route.ts
@@ -1,18 +1,39 @@
 import { auth } from "~/auth";
 import type { UserWithEmailVerifiedAndPasswordHash } from "~/lib/auth/types";
+import { checkOAuthAccountAlreadyLinkedByUserIdAndProvider } from "~/services/database/queries/auth/oauth-accounts";
 import { NextResponse } from "next/server";
 
-// TODO
-// Try linking multiple github accounts, should only be able to link one github account per user
 export async function GET(): Promise<NextResponse> {
   const session = await auth();
 
   const user = <UserWithEmailVerifiedAndPasswordHash>session?.user;
 
+  const userId = user?.id;
   const userEmail = user?.email;
 
   if (!userEmail || !user?.emailVerified || !user?.password_hash) {
     throw new Error("Invalid session");
+  }
+
+  try {
+    if (typeof userId === "number") {
+      const provider = "github";
+
+      const oAuthAccountAlreadyLinked =
+        await checkOAuthAccountAlreadyLinkedByUserIdAndProvider(
+          userId,
+          provider,
+        );
+
+      if (oAuthAccountAlreadyLinked) {
+        throw new Error("Not allowed to link OAuth account");
+      }
+    } else {
+      throw new Error("Incorrect user id data type");
+    }
+  } catch (err) {
+    console.error(err);
+    throw new Error("Failed to link OAuth account");
   }
 
   return NextResponse.json(
