b9eb012e9729bab9e1c8f501eadff18867e55a0b
J the Code Monkey <jaythecodemonkey.7vk7i@slmail.me>
2025-07-05 10:28:40 -0400

    style: give formation sidebar trigger a fixed position to prevent it from scrolling with the page content

--- src/app/(site)/formation/layout.tsx
+++ src/app/(site)/formation/layout.tsx
@@ -1,5 +1,5 @@
 import { auth } from "~/auth";
-import { SidebarProvider, SidebarTrigger } from "~/components/ui/sidebar";
+import { SidebarProvider } from "~/components/ui/sidebar";
 import { FormationSidebar } from "~/features/sidebars/formation";
 import { FullPageLoadingSpinner } from "~/features/spinners";
 import { redirect } from "next/navigation";
@@ -23,7 +23,6 @@ export default async function FormationLayout({
       {session && (
         <SidebarProvider>
           <FormationSidebar />
-          <SidebarTrigger className="ml-2 mt-2 p-3" />
           <div className="flex grow items-center justify-center py-5 md:items-start md:justify-start md:px-6 md:pt-0 min-[800px]:px-10 min-[850px]:px-16">
             {children}
           </div>
--- src/features/sidebars/formation/components/FormationSidebar.tsx
+++ src/features/sidebars/formation/components/FormationSidebar.tsx
@@ -17,6 +17,8 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
   SidebarSeparator,
+  SidebarTrigger,
+  useSidebar,
 } from "~/components/ui/sidebar";
 import { COLLAPSIBLE_TRIGGERS } from "~/features/sidebars/formation/constants/constants";
 import { ChevronDown } from "lucide-react";
@@ -26,65 +28,75 @@ import { usePathname } from "next/navigation";
 export function FormationSidebar() {
   const pathname = usePathname();
 
+  const { state, isMobile } = useSidebar();
+
   return (
-    <Sidebar className="md:top-[65px] md:pb-[65px] min-[1020px]:top-[73px] min-[1020px]:pb-[73px]">
-      <SidebarHeader>Form 1023</SidebarHeader>
-      <SidebarSeparator />
-      <SidebarContent>
-        {COLLAPSIBLE_TRIGGERS.map((collapsibleTrigger, index) => (
-          <Collapsible
-            key={collapsibleTrigger.title}
-            defaultOpen={pathname.startsWith(collapsibleTrigger.urlBase)}
-            className="group/collapsible"
-          >
-            <SidebarGroup>
-              <SidebarGroupLabel className="justify-between text-left" asChild>
-                <CollapsibleTrigger>
-                  {collapsibleTrigger.title}
-                  <ChevronDown className="ml-3 transition-transform group-data-[state=open]/collapsible:rotate-180" />
-                  <span className="sr-only">Toggle</span>
-                </CollapsibleTrigger>
-              </SidebarGroupLabel>
-              <CollapsibleContent>
-                <SidebarGroupContent>
-                  <SidebarMenu>
-                    {collapsibleTrigger.sidebarGroupContent.map((item) => (
-                      <SidebarMenuItem key={item.step}>
-                        <SidebarMenuButton
-                          asChild
-                          isActive={item.url === pathname}
-                          className="h-full data-[active=true]:bg-sidebar-primary/80 data-[active=true]:focus-visible:ring-ringPrimary/80 dark:data-[active=true]:bg-sidebar-primary/40 dark:data-[active=true]:focus-visible:ring-ringPrimary/40"
-                        >
-                          <Link
-                            className="my-1"
-                            href={{
-                              pathname: item.url,
-                            }}
+    <>
+      <Sidebar className="md:top-[65px] md:pb-[65px] min-[1020px]:top-[73px] min-[1020px]:pb-[73px]">
+        <SidebarHeader>Form 1023</SidebarHeader>
+        <SidebarSeparator />
+        <SidebarContent>
+          {COLLAPSIBLE_TRIGGERS.map((collapsibleTrigger, index) => (
+            <Collapsible
+              key={collapsibleTrigger.title}
+              defaultOpen={pathname.startsWith(collapsibleTrigger.urlBase)}
+              className="group/collapsible"
+            >
+              <SidebarGroup>
+                <SidebarGroupLabel
+                  className="justify-between text-left"
+                  asChild
+                >
+                  <CollapsibleTrigger>
+                    {collapsibleTrigger.title}
+                    <ChevronDown className="ml-3 transition-transform group-data-[state=open]/collapsible:rotate-180" />
+                    <span className="sr-only">Toggle</span>
+                  </CollapsibleTrigger>
+                </SidebarGroupLabel>
+                <CollapsibleContent>
+                  <SidebarGroupContent>
+                    <SidebarMenu>
+                      {collapsibleTrigger.sidebarGroupContent.map((item) => (
+                        <SidebarMenuItem key={item.step}>
+                          <SidebarMenuButton
+                            asChild
+                            isActive={item.url === pathname}
+                            className="h-full data-[active=true]:bg-sidebar-primary/80 data-[active=true]:focus-visible:ring-ringPrimary/80 dark:data-[active=true]:bg-sidebar-primary/40 dark:data-[active=true]:focus-visible:ring-ringPrimary/40"
                           >
-                            <Badge
-                              className={`mr-1 focus-visible:outline-none ${item.url === pathname ? "bg-transparent text-primary-foreground hover:bg-transparent focus:ring-transparent focus:ring-offset-transparent" : "bg-sidebar-accent hover:bg-sidebar-accent"}`}
+                            <Link
+                              className="my-1"
+                              href={{
+                                pathname: item.url,
+                              }}
                             >
-                              {item.step}
-                            </Badge>
-                            <div
-                              className={`text-xs outline-none ${item.url === pathname ? "text-primary-foreground" : "text-foreground"}`}
-                            >
-                              {item.title}
-                            </div>
-                          </Link>
-                        </SidebarMenuButton>
-                      </SidebarMenuItem>
-                    ))}
-                  </SidebarMenu>
-                </SidebarGroupContent>
-              </CollapsibleContent>
-            </SidebarGroup>
-            <SidebarSeparator
-              className={`${COLLAPSIBLE_TRIGGERS.length - 1 === index && "hidden"}`}
-            />
-          </Collapsible>
-        ))}
-      </SidebarContent>
-    </Sidebar>
+                              <Badge
+                                className={`mr-1 focus-visible:outline-none ${item.url === pathname ? "bg-transparent text-primary-foreground hover:bg-transparent focus:ring-transparent focus:ring-offset-transparent" : "bg-sidebar-accent hover:bg-sidebar-accent"}`}
+                              >
+                                {item.step}
+                              </Badge>
+                              <div
+                                className={`text-xs outline-none ${item.url === pathname ? "text-primary-foreground" : "text-foreground"}`}
+                              >
+                                {item.title}
+                              </div>
+                            </Link>
+                          </SidebarMenuButton>
+                        </SidebarMenuItem>
+                      ))}
+                    </SidebarMenu>
+                  </SidebarGroupContent>
+                </CollapsibleContent>
+              </SidebarGroup>
+              <SidebarSeparator
+                className={`${COLLAPSIBLE_TRIGGERS.length - 1 === index && "hidden"}`}
+              />
+            </Collapsible>
+          ))}
+        </SidebarContent>
+      </Sidebar>
+      <SidebarTrigger
+        className={`fixed top-[65px] ml-2 mt-2 p-3 md:transition-[left] md:duration-200 md:ease-linear min-[1020px]:top-[73px] ${state === "expanded" && !isMobile ? "left-[255px]" : "left-0"}`}
+      />
+    </>
   );
 }
