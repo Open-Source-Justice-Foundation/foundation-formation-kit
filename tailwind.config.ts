4bac42a6ee01f9851a7e93f4c6f29e4978dcbc3c
J the Code Monkey <jaythecodemonkey.7vk7i@slmail.me>
2025-07-07 14:59:25 -0400

    style: remove unused container setting in tailwind config and explicitly add screen breakpoints to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,12 +5,12 @@ const config: Config = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   prefix: "",
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1440px",
-      },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
     },
     extend: {
       colors: {
