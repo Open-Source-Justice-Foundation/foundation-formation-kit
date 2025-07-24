fb1ce574e498710e1f1fc2f0db2afda4403508c6
J the Code Monkey <jaythecodemonkey.7vk7i@slmail.me>
2025-07-24 14:04:05 -0400

    feat: update formation schedule e step 1

--- src/app/(site)/formation/schedule-e/step-1/page.tsx
+++ src/app/(site)/formation/schedule-e/step-1/page.tsx
@@ -14,14 +14,15 @@ import {
   FormMessage,
 } from "~/components/ui/form";
 import { RadioGroup, RadioGroupItem } from "~/components/ui/radio-group";
+import { Textarea } from "~/components/ui/textarea";
 import { FormationNavigationButtons } from "~/features/formation/components/FormationNavigationButtons";
-import { form1023ScheduleEYesNoRadioSchema } from "~/lib/formation/validation/schedule-e/schemas";
+import { form1023ScheduleEPart1Schema } from "~/lib/formation/validation/schedule-e/schemas";
 import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 import { z } from "zod";
 
-type FormValues = z.infer<typeof form1023ScheduleEYesNoRadioSchema>;
+type FormValues = z.infer<typeof form1023ScheduleEPart1Schema>;
 
 export default function FormationScheduleEStep1Page() {
   const router = useRouter();
@@ -29,12 +30,12 @@ export default function FormationScheduleEStep1Page() {
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const form = useForm<FormValues>({
-    resolver: zodResolver(form1023ScheduleEYesNoRadioSchema),
+    resolver: zodResolver(form1023ScheduleEPart1Schema),
   });
 
   async function onSubmit(values: FormValues) {
     setIsLoading(true);
-    const { radioInput } = values;
+    const { input1, input2, input3 } = values;
 
     const url = "/api/formation/schedule-e/step-1";
     let response: Response = new Response();
@@ -46,7 +47,9 @@ export default function FormationScheduleEStep1Page() {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          radioInput,
+          input1,
+          input2,
+          input3,
         }),
       });
 
@@ -81,7 +84,7 @@ export default function FormationScheduleEStep1Page() {
           >
             <FormField
               control={form.control}
-              name="radioInput"
+              name="input1"
               render={({ field }) => (
                 <FormItem>
                   <FormDescription>
@@ -124,6 +127,143 @@ export default function FormationScheduleEStep1Page() {
                       </FormItem>
                     </RadioGroup>
                   </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="input2"
+              render={({ field }) => (
+                <FormItem>
+                  <FormDescription>
+                    Revenue Procedure 2014-11, 2014-1 C.B. 411, provides
+                    procedures for reinstating your tax-exempt status. Select
+                    the section of Revenue Procedure 2014-11 under which you
+                    want us to consider your reinstatement request.
+                  </FormDescription>
+                  <FormControl>
+                    <RadioGroup
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                      className="flex flex-col"
+                    >
+                      <FormItem>
+                        <div className="flex items-center gap-3">
+                          <FormControl>
+                            <RadioGroupItem
+                              value="Section 4. You are seeking retroactive reinstatement under section 4 of Revenue Procedure 2014-11. By selecting this line, you attest
+that you meet the specified requirements of section 4, that your failure to file was not intentional, and that you have put in place
+procedures to file required returns or notices in the future. Do not complete the rest of Schedule E."
+                              className="focus-visible:ring-ringPrimary"
+                              disabled={isLoading}
+                            />
+                          </FormControl>
+                          <FormLabel className="text-sm font-normal sm:text-base">
+                            Section 4. You are seeking retroactive reinstatement
+                            under section 4 of Revenue Procedure 2014-11. By
+                            selecting this line, you attest that you meet the
+                            specified requirements of section 4, that your
+                            failure to file was not intentional, and that you
+                            have put in place procedures to file required
+                            returns or notices in the future. Do not complete
+                            the rest of Schedule E.
+                          </FormLabel>
+                        </div>
+                      </FormItem>
+                      <FormItem>
+                        <div className="flex items-center gap-3">
+                          <FormControl>
+                            <RadioGroupItem
+                              value="Section 5. You are seeking retroactive reinstatement under section 5 of Revenue Procedure 2014-11. By selecting this line, you attest
+that you meet the specified requirements of section 5, that you have filed required annual returns, that your failure to file was not
+intentional, and that you have put in place procedures to file required returns or notices in the future."
+                              className="focus-visible:ring-ringPrimary"
+                              disabled={isLoading}
+                            />
+                          </FormControl>
+                          <FormLabel className="text-sm font-normal sm:text-base">
+                            Section 5. You are seeking retroactive reinstatement
+                            under section 5 of Revenue Procedure 2014-11. By
+                            selecting this line, you attest that you meet the
+                            specified requirements of section 5, that you have
+                            filed required annual returns, that your failure to
+                            file was not intentional, and that you have put in
+                            place procedures to file required returns or notices
+                            in the future.
+                          </FormLabel>
+                        </div>
+                      </FormItem>
+                      <FormItem>
+                        <div className="flex items-center gap-3">
+                          <FormControl>
+                            <RadioGroupItem
+                              value="Section 6. You are seeking retroactive reinstatement under section 6 of Revenue Procedure 2014-11. By selecting this line, you attest
+that you meet the specified requirements of section 6, that you have filed required annual returns, that your failure to file was not
+intentional, and that you have put in place procedures to file required returns or notices in the future."
+                              className="focus-visible:ring-ringPrimary"
+                              disabled={isLoading}
+                            />
+                          </FormControl>
+                          <FormLabel className="text-sm font-normal sm:text-base">
+                            Section 6. You are seeking retroactive reinstatement
+                            under section 6 of Revenue Procedure 2014-11. By
+                            selecting this line, you attest that you meet the
+                            specified requirements of section 6, that you have
+                            filed required annual returns, that your failure to
+                            file was not intentional, and that you have put in
+                            place procedures to file required returns or notices
+                            in the future.
+                          </FormLabel>
+                        </div>
+                      </FormItem>
+                      <FormItem>
+                        <div className="flex items-center gap-3">
+                          <FormControl>
+                            <RadioGroupItem
+                              value="Section 7. You are seeking reinstatement under section 7 of Revenue Procedure 2014-11, effective the date you are filling this application. Do not complete the rest of Schedule E."
+                              className="focus-visible:ring-ringPrimary"
+                              disabled={isLoading}
+                            />
+                          </FormControl>
+                          <FormLabel className="text-sm font-normal sm:text-base">
+                            Section 7. You are seeking reinstatement under
+                            section 7 of Revenue Procedure 2014-11, effective
+                            the date you are filling this application. Do not
+                            complete the rest of Schedule E.
+                          </FormLabel>
+                        </div>
+                      </FormItem>
+                    </RadioGroup>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="input3"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>
+                    Attempts to Comply with Filing Requirements
+                  </FormLabel>
+                  <FormControl>
+                    <Textarea
+                      placeholder="Describe how you exercised ordinary business care and prudence in determining and attempting to comply with your filing requirements..."
+                      className="resize-none text-sm focus-visible:ring-ringPrimary"
+                      {...field}
+                      disabled={isLoading}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    Describe how you exercised ordinary business care and
+                    prudence in determining and attempting to comply with your
+                    filing requirements in at least one of the three years of
+                    revocation and the steps you have taken or will take to
+                    avoid or mitigate future failures to file timely returns or
+                    notices. Do not complete the rest of Schedule E.
+                  </FormDescription>
                   <FormMessage />
                 </FormItem>
               )}
--- src/lib/formation/validation/schedule-e/schemas.ts
+++ src/lib/formation/validation/schedule-e/schemas.ts
@@ -1,7 +1,35 @@
-import { object, z } from "zod";
+import { object, string, z } from "zod";
 
 export const form1023ScheduleEYesNoRadioSchema = object({
   radioInput: z.enum(["Yes", "No"], {
     required_error: "Required",
   }),
+});
+
+export const form1023ScheduleEPart1Schema = object({
+  input1: z.enum(["Yes", "No"], {
+    required_error: "Required",
+  }),
+  input2: z.enum(
+    [
+      "Section 4. You are seeking retroactive reinstatement under section 4 of Revenue Procedure 2014-11. By selecting this line, you attest that you meet the specified requirements of section 4, that your failure to file was not intentional, and that you have put in place procedures to file required returns or notices in the future. Do not complete the rest of Schedule E.",
+      "Section 5. You are seeking retroactive reinstatement under section 5 of Revenue Procedure 2014-11. By selecting this line, you attest that you meet the specified requirements of section 5, that you have filed required annual returns, that your failure to file was not intentional, and that you have put in place procedures to file required returns or notices in the future.",
+      "Section 6. You are seeking retroactive reinstatement under section 6 of Revenue Procedure 2014-11. By selecting this line, you attest that you meet the specified requirements of section 6, that you have filed required annual returns, that your failure to file was not intentional, and that you have put in place procedures to file required returns or notices in the future.",
+      "Section 7. You are seeking reinstatement under section 7 of Revenue Procedure 2014-11, effective the date you are filling this application. Do not complete the rest of Schedule E.",
+    ],
+    {
+      required_error: "Required",
+    },
+  ),
+  input3: string({
+    required_error: "Required",
+    invalid_type_error: "Input must be a string",
+  })
+    .nonempty({
+      message: "Required",
+    })
+    .max(1000, {
+      message: "Input can be at most 1000 characters",
+    })
+    .trim(),
 });
